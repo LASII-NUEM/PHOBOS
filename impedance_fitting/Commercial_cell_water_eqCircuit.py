bfffe5a4403c580b860f34aecce1835fc45566fc
Everton <evertonjunior@alunos.utfpr.edu.br>
2026-01-21 13:35:13 -0300

    [Framework] Add data2Zview.py this conde handle the test data and creates a file to be uploaded on fitting apps.

---
+++ impedance_fitting/Commercial_cell_water_eqCircuit.py
@@ -0,0 +1,68 @@
+from framework import file_lcr, characterization_utils
+import numpy as np
+from impedance.models.circuits import Randles, CustomCircuit
+from impedance.visualization import plot_nyquist
+# import matplotlib
+# matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+
+def MSE(z_hat, z):
+    SSE = np.sum((np.abs(z)-np.abs(z_hat))**2) #sum of squared errors
+    return SSE/len(z_hat)
+
+# LCR spectroscopy acquisition
+LCR_spec_air_obj = file_lcr.read('../data/test_media_12_01/c0.csv', n_samples=3, sweeptype="cell", acquisition_mode="spectrum", aggregate=np.mean)
+LCR_spec_c1_obj = file_lcr.read('../data/test_media_12_01/tap.csv', n_samples=3, sweeptype="cell", acquisition_mode="spectrum", aggregate=np.mean)
+
+#dielectric parameters
+c1_eps_real, c1_eps_imag = characterization_utils.dielectric_params_corrected(LCR_spec_c1_obj, LCR_spec_air_obj, LCR_spec_c1_obj.freqs) #compute the spectrum based on the experimental data
+c1_z_real, c1_z_imag = characterization_utils.complex_impedance(LCR_spec_c1_obj, LCR_spec_c1_obj.freqs) #compute the complex impedance based on the experimental data
+c1_z = c1_z_real - 1j*c1_z_imag
+
+initial_guess = [80.45, 1, 100, 3320, 1e-3, 1e3, 1]
+circuit = CustomCircuit(initial_guess=initial_guess,
+                              circuit='R_0-p(Wo_1,CPE_1)-p(R_2,C_1)')
+
+circuit.fit(LCR_spec_c1_obj.freqs, c1_z)
+print(circuit)
+
+opt_fit = circuit.predict(LCR_spec_c1_obj.freqs)
+curr_MSE = MSE(c1_z, opt_fit) #compute the MSE (option - impedance.models.circuits.fitting.rmse)
+print(curr_MSE)
+
+#plot
+plt.figure()
+leg = []
+plt.scatter(c1_z_real, c1_z_imag, marker='o', color="tab:blue")
+leg.append('c1 measured')
+plt.plot(opt_fit.real, -opt_fit.imag, color="tab:orange")
+leg.append('c1 model (impedance.py)')
+plt.xlabel("Z'")
+plt.ylabel("Z''")
+plt.legend(leg)
+plt.grid()
+plt.show()
+
+plt.figure()
+plt.subplot(1,2,1)
+leg = []
+plt.scatter(np.log10(LCR_spec_c1_obj.freqs), np.abs(c1_z))
+leg.append('c1 measured')
+plt.plot(np.log10(LCR_spec_c1_obj.freqs), np.abs(opt_fit), color="tab:orange")
+leg.append('c1 measured')
+plt.ylabel("|Z|")
+plt.xlabel("log(Frequency)")
+plt.legend(leg)
+plt.grid()
+
+plt.subplot(1,2,2)
+leg = []
+plt.scatter(np.log10(LCR_spec_c1_obj.freqs), -np.angle(c1_z.astype('complex')))
+leg.append('c1 measured')
+plt.plot(np.log10(LCR_spec_c1_obj.freqs), -np.angle(opt_fit), color="tab:orange")
+leg.append('c1 measured')
+plt.ylabel("-∠Z")
+plt.xlabel("log(Frequency)")
+plt.legend(leg)
+plt.grid()
+plt.show()
---
+++ impedance_fitting/data2Zview.py
@@ -0,0 +1,63 @@
+from framework import file_lcr,file_ia, characterization_utils
+import numpy as np
+import csv
+import os
+
+def savecsv(a, b, c,filename, folder, header=("Freq (Hz)","Z(real)","Z(imag)")):
+
+    a, b, c = map(np.ravel, map(np.asarray, (a, b, c)))
+    if not (len(a) == len(b) == len(c)):
+        raise ValueError("arrays must have same length")
+
+    os.makedirs(folder, exist_ok=True)
+    path = os.path.join(folder, f"{filename}.csv")
+
+    with open(path, "w", newline="", encoding="ascii") as f:
+        w = csv.writer(f)
+        if header: w.writerow(header)
+        w.writerows(zip(a, b, c))
+    return
+
+#Impedance Analyzer load acquisition
+spec_ia_obj = file_ia.read('../data/test_media_13_01/4294A_DataTransfer_0310.xls')
+folderZview_IA = '../data/test_media_13_01/Zview'
+
+IA_spec_air_obj = spec_ia_obj["c0"]
+IA_spec_tap_obj= spec_ia_obj["tap"]
+IA_spec_mineral_obj= spec_ia_obj["mineral"]
+IA_spec_distilled_obj= spec_ia_obj["distilled"]
+IA_spec_deionized_obj= spec_ia_obj["deionized"]
+
+IA_freqs = IA_spec_air_obj.freqs
+
+IA_tap_eps_real, IA_tap_eps_imag = characterization_utils.dielectric_params_corrected(IA_spec_tap_obj, IA_spec_air_obj, IA_freqs) #compute the spectrum based on the experimental data
+IA_tap_z_real,   IA_tap_z_imag = characterization_utils.complex_impedance(IA_spec_tap_obj,IA_freqs) #compute the complex impedance based on the experimental data
+
+IA_mineral_eps_real, IA_mineral_eps_imag = characterization_utils.dielectric_params_corrected(IA_spec_mineral_obj, IA_spec_air_obj, IA_freqs) #compute the spectrum based on the experimental data
+IA_mineral_z_real,   IA_mineral_z_imag = characterization_utils.complex_impedance(IA_spec_mineral_obj, IA_freqs) #compute the complex impedance based on the experimental data
+
+IA_distilled_eps_real, IA_distilled_eps_imag = characterization_utils.dielectric_params_corrected(IA_spec_distilled_obj, IA_spec_air_obj, IA_freqs) #compute the spectrum based on the experimental data
+IA_distilled_z_real, IA_distilled_z_imag = characterization_utils.complex_impedance(IA_spec_distilled_obj, IA_freqs) #compute the complex impedance based on the experimental data
+
+IA_deionized_eps_real, IA_deionized_eps_imag = characterization_utils.dielectric_params_corrected(IA_spec_deionized_obj, IA_spec_air_obj, IA_freqs) #compute the spectrum based on the experimental data
+IA_deionized_z_real, IA_deionized_z_imag = characterization_utils.complex_impedance(IA_spec_deionized_obj, IA_freqs) #compute the complex impedance based on the experimental data
+
+savecsv(IA_freqs,IA_tap_z_real, IA_tap_z_imag, filename = "IA_tap", folder = folderZview_IA)
+savecsv(IA_freqs,IA_mineral_z_real, IA_mineral_z_imag, filename = "IA_mineral", folder = folderZview_IA)
+savecsv(IA_freqs,IA_distilled_z_real, IA_distilled_z_imag, filename = "IA_distilled", folder = folderZview_IA)
+savecsv(IA_freqs,IA_deionized_z_real, IA_deionized_z_imag, filename = "IA_deionized", folder = folderZview_IA)
+
+# # LCR spectroscopy acquisition
+LCR_spec_air_obj = file_lcr.read('../data/test_media_12_01/c0.csv', n_samples=3, sweeptype="cell", acquisition_mode="spectrum", aggregate=np.mean)
+LCR_spec_tap_obj = file_lcr.read('../data/test_media_12_01/tap.csv', n_samples=3, sweeptype="cell", acquisition_mode="spectrum", aggregate=np.mean)
+
+folderZview_LCR = '../data/test_media_12_01/Zview'
+
+
+LCR_freqs = LCR_spec_air_obj.freqs
+#dielectric parameters
+LCR_tap_eps_real, LCR_tap_eps_imag = characterization_utils.dielectric_params_corrected(LCR_spec_tap_obj, LCR_spec_air_obj, LCR_freqs) #compute the spectrum based on the experimental data
+LCR_tap_z_real, LCR_tap_z_imag = characterization_utils.complex_impedance(LCR_spec_tap_obj, LCR_freqs) #compute the complex impedance based on the experimental data
+
+savecsv(LCR_freqs,LCR_tap_z_real, LCR_tap_z_imag, filename = "LCR_tap", folder = folderZview_LCR)
+
